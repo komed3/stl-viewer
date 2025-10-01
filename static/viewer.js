868a179c8d6e085ac0989cce366f0a9c9ab22220
komed3 <pk97@outlook.de>
2025-10-01 10:51:06 +0200

    init canvas scene

--- static/viewer.js
+++ static/viewer.js
@@ -31,6 +31,39 @@ class Model3DViewer {
 
     }
 
+    init () {
+
+        // Create scene
+        this.scene = new THREE.Scene();
+        this.scene.background = new THREE.Color( 0xf8f9fa );
+
+        // Create camera
+        const canvas = document.getElementById( 'canvas' );
+        const aspect = canvas.clientWidth / canvas.clientHeight;
+        this.camera = new THREE.PerspectiveCamera( 75, aspect, 0.1, 1000 );
+        this.camera.position.set( 5, 5, 5 );
+
+        // Create renderer
+        this.renderer = new THREE.WebGLRenderer( { 
+            canvas: canvas, antialias: true,
+            preserveDrawingBuffer: true
+        } );
+
+        this.renderer.setSize( canvas.clientWidth, canvas.clientHeight );
+        this.renderer.setPixelRatio( window.devicePixelRatio );
+        this.renderer.shadowMap.enabled = true;
+        this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+        // Create controls
+        this.controls = new THREE.OrbitControls( this.camera, this.renderer.domElement );
+        this.controls.enableDamping = true;
+        this.controls.dampingFactor = 0.05;
+        this.controls.screenSpacePanning = false;
+        this.controls.minDistance = 1;
+        this.controls.maxDistance = 100;
+
+    }
+
 }
 
 document.addEventListener( 'DOMContentLoaded', () => new Model3DViewer () );
